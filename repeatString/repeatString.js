77e7b38ec2abb18cbb0f78689703418e2f5e6001
Chris Mason <c_mason88@hotmail.co.uk>
2018-03-03 13:44:56 +0000

    completed HelloWorld and repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-var repeatString = function() {
-
+var repeatString = function(inputString, numRepeat) {
+ let outputString = "";
+ if (numRepeat<0) {
+   return "ERROR";
+ }
+ for (let i=0; i < numRepeat; i++) {
+   outputString = outputString + inputString;
+ }
+ return outputString;
 }
 
 module.exports = repeatString
