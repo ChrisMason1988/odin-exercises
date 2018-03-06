8efc71302994d7e4fb69ff5ff24370fd85ebaeb6
Chris Mason <c_mason88@hotmail.co.uk>
2018-03-06 11:57:51 +0000

    completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-var removeFromArray = function() {
-
+var removeFromArray = function(inputArray) {
+ for (i=1; i<arguments.length; i++) {
+   for (j=0; j<inputArray.length; j++) {
+     if (arguments[i] === inputArray[j]) {
+       inputArray.splice (j,1)
+     }
+   }
+ }
+ return inputArray
 }
 
 module.exports = removeFromArray
