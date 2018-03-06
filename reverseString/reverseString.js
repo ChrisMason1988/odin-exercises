cb40c061042314d9900233d6a19864c938561bae
Chris Mason <c_mason88@hotmail.co.uk>
2018-03-06 11:14:27 +0000

    completed reverseString

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,11 @@
-var reverseString = function() {
-
+var reverseString = function(inputString) {
+ let outputString = "";
+ let i=inputString.length -1;
+ while (i>=0) {
+   outputString = outputString + inputString.charAt (i);
+   i--;
+ }
+ return outputString
 }
 
 module.exports = reverseString
