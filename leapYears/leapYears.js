889cedc5ec777e56d1399e54da89a47cd984610c
Chris Mason <c_mason88@hotmail.co.uk>
2018-03-07 11:47:42 +0000

    completed leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,4 +1,21 @@
-var leapYears = function() {
+var leapYears = function(year) {
+  //This function determines whether the year is a leap year
+  //If the year is divisable by 4, then it is a leap year
+  if (year % 4 == 0) {
+    //But if it is divisable by 100 and not divisable by 400 then it is not a leap year
+    if (year % 100 == 0 && year % 400 !=0) {
+      //This is not a leap year
+      return false
+    }
+    else {
+      //This is a leap year
+      return true
+    }
+  }
+  else {
+    //Not divisable by 4, hence its never a leap year
+    return false
+  }
 
 }
 
