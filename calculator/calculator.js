448601cfacd1ef513cb7079b9bea8a9d2c895dce
Chris Mason <c_mason88@hotmail.co.uk>
2018-05-09 20:20:45 +0100

    completed calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,41 @@
-function add () {
-	
+function add (n1, n2) {
+	return n1 + n2;
 }
 
-function subtract () {
-	
+function subtract (n1, n2) {
+  return n1 - n2;
 }
 
-function sum () {
-	
+function sum (numberArray) {
+  var answer=0;
+	// loop through each element in array, adding it to the variable answer.
+	for (var i=0; i<numberArray.length; i++) {
+		answer=answer+numberArray[i];
+	}
+	return answer;
 }
 
-function multiply () {
-	
+function multiply (numberArray) {
+	var answer=1;
+	// loop through each element in array, multiply by the variable answer.
+	// answer starts at 1 because we cant multiply by 0.
+	for (var i=0; i<numberArray.length; i++) {
+		answer=answer*numberArray[i];
+	}
+	return answer;
 }
 
-function power() {
-	
+function power(n1, n2) {
+  return Math.pow (n1, n2);
 }
 
-function factorial() {
-	
+function factorial(n1) {
+  var answer=1;
+	// Multiply all numbers from 1 up until n1.
+	for (var i=1; i<=n1; i++) {
+		answer=answer*i;
+	}
+	return answer;
 }
 
 module.exports = {
