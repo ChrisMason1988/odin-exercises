2ede3a484b94771a5d8f8285f74aea82a948597d
Chris Mason <c_mason88@hotmail.co.uk>
2018-06-12 12:06:12 +0100

    Completed Palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,30 @@
-var palindromes = function() {
-
+var reverseString = function(inputString) {
+  //This function reverses the inputString
+ let outputString = "";
+ //Start at the end of the string
+ let i=inputString.length -1;
+ //Work backwards through the string, copying it to outputString
+ while (i>=0) {
+   outputString = outputString + inputString.charAt (i);
+   i--;
+ }
+ return outputString
+}
+// Checking whether a string is a Palindrome.
+var palindromes = function(inputString) {
+  // Convert it all to the same case.
+    var forwardString = inputString.toLowerCase();
+    // Remove special characters.
+    forwardString = forwardString.replace(/[.,\/#!$%\^&\*;:{}=\-_ `~()]/g,"");
+    // Create a backwards version of it.
+    var backwardString = reverseString(forwardString);
+    // Compare the forwards version and backwards version. If same return true.
+    if (backwardString == forwardString) {
+      return true
+    }
+    else {
+      return false
+    }
 }
 
 module.exports = palindromes
